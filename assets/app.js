98a750b8222553acad3e073b8107828f26610bb2
mohanadqasim <mohanad.qasim@hotmail.com>
2023-02-05 21:42:03 +0000

    js

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,14 @@
-let person = prompt("Please enter your name");
+let person = prompt("Please enter your name...");
 
-let gender = prompt("please enter your gender (male or female)");
+let gender = prompt("please enter your gender (male or female)...");
 
-let age = prompt("please enter your age");
+let age = prompt("please enter your age...");
 
 if (age <= 0) {
     alert("you have entered a wrong value :(");
 }
 
-let welcomeMessage = confirm("do you want to see welcoming message");
+let welcomeMessage = confirm("do you want to see welcoming message?");
 
 if (welcomeMessage == true) {
     if (gender === "male") {
